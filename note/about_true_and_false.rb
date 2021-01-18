d2ab32e94798992d436d9e43666fc57bff5094c3
gy <372301467@qq.com>
2021-01-18 09:26:02 +0800

    about_true_and_false

--- note/about_true_and_false.rb
+++ note/about_true_and_false.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutTrueAndFalse < Neo::Koan
-  def truth_value(condition)
+  def truth_value(condition) ## 注意，下面的会用到这个函数
     if condition
       :true_stuff
     else
@@ -10,24 +10,24 @@ class AboutTrueAndFalse < Neo::Koan
   end
 
   def test_true_is_treated_as_true
-    assert_equal __, truth_value(true)
+    assert_equal :true_stuff, truth_value(true)
   end
 
   def test_false_is_treated_as_false
-    assert_equal __, truth_value(false)
+    assert_equal :false_stuff, truth_value(false)
   end
 
   def test_nil_is_treated_as_false_too
-    assert_equal __, truth_value(nil)
+    assert_equal :false_stuff, truth_value(nil)
   end
 
   def test_everything_else_is_treated_as_true
-    assert_equal __, truth_value(1)
-    assert_equal __, truth_value(0)
-    assert_equal __, truth_value([])
-    assert_equal __, truth_value({})
-    assert_equal __, truth_value("Strings")
-    assert_equal __, truth_value("")
+    assert_equal :true_stuff, truth_value(1)
+    assert_equal :true_stuff, truth_value(0) ## !
+    assert_equal :true_stuff, truth_value([])
+    assert_equal :true_stuff, truth_value({})
+    assert_equal :true_stuff, truth_value("Strings")
+    assert_equal :true_stuff, truth_value("") ## !
   end
 
 end
