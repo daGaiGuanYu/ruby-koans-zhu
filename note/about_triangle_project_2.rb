5ce0cbd64885d04b3ab29b409d14787f7e7730c5
gy <372301467@qq.com>
2021-01-18 10:15:22 +0800

    triangle2

--- note/about_triangle_project_2.rb
+++ note/about_triangle_project_2.rb
@@ -1,11 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # You need to write the triangle method in the file 'triangle.rb'
+## 你需要写（同目录下的）triangle.rb 文件里的 triangle 方法
 require './triangle.rb'
 
 class AboutTriangleProject2 < Neo::Koan
   # The first assignment did not talk about how to handle errors.
+  ## 第一部分未讨论如何处理错误。
   # Let's handle that part now.
+  ## 现在来处理一下。（为了和上一个 三角形 题目区分，我在下面写了个 triangle2 方法）
   def test_illegal_triangles_throw_exceptions
     assert_raise(TriangleError) do triangle(0, 0, 0) end
     assert_raise(TriangleError) do triangle(3, 4, -5) end
--- note/triangle.rb
+++ note/triangle.rb
@@ -16,11 +16,17 @@
 ##                没有相等的边
 #
 # The tests for this method can be found in
+## 这个方法的测试，可以在以下文件中找到
 #   about_triangle_project.rb
 # and
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  ## 这个方法，在两个题目中用到，第一题，值判断三角形的类型
+  ## 第二题要在判断之前，检查这个三角形是否是“合法的三角形”，即“每条边的边长都大于零、每两条边都大于第三边”
+  ## 做第一题时，先忽略前两行代码
+  a, b, c = [a, b, c].sort ## 排序过后，a 最小，c 最大
+  raise TriangleError if (a <= 0 || a + b <= c)
   # WRITE THIS CODE
   if a == b && b == c
     :equilateral
@@ -29,9 +35,16 @@ def triangle(a, b, c)
   else
     :scalene
   end
-  ## 未考虑“不合法的三角形”
-  ## 没有绝对的完美，只有合适
 end
+
+## 在 ruby china 上看到这样一个解答（我做了一点点修改，原文链接：https://ruby-china.org/topics/15476）
+# def triangle a, b, c
+#   [:equilateral,:isosceles,:scalene][[a,b,c].uniq.size - 1]
+# end
+## 这种写法，虽然没什么意义，但很有意思
+## 代码可读性太低（感觉运行效率也低一点）
+## 但，用一行解决，还是很值得玩味
+## 有人说这是“优雅”，但我更愿意称之为“花里胡哨”
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
